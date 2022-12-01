79163b6356b2fc451aba7b24663f919f7b7d1815
mnct-mito <71414392+mnct-mito@users.noreply.github.com>
2022-12-01 16:07:39 +0900

    Create code01.ino

---
+++ code01.ino
@@ -0,0 +1,19 @@
+#include <Zumo32U4.h>
+
+#define MaxSpeed 400
+
+Zumo32U4ButtonA buttonA;
+Zumo32U4Motors motors;
+Zumo32U4LCD lcd;
+
+void setup()
+{
+    lcd.clear();
+    lcd.print("Press A");
+}
+
+void loop()
+{
+    /*この部分にプリントのコードを入力しなさい*/
+
+}
